4e6296aaa7004f0863124e76b5a20839c1657ab3
paomiange <1029456341@qq.com>
2017-02-16 11:51:25 +0800

    442.Find All Duplicates in an Array找出数组中1到n重复出现的数字

---
+++ 442.Find All Duplicates in an Array/Solution.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+	public List<Integer> findDuplicates(int[] nums) {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			int index = Math.abs(nums[i]) - 1;
+			if (nums[index] < 0) {
+				list.add(index + 1);
+			}
+			nums[index] *= -1;
+		}
+		return list;
+	}
+}
