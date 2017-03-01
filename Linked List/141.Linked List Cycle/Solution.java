66544eb3d2971a49ef185f14b1b9e29ac9c1d415
paomiange <1029456341@qq.com>
2017-03-01 10:43:13 +0800

    判断单链表是否有环

---
+++ Linked List/141.Linked List Cycle/Solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+	public boolean hasCycle(ListNode head) {
+		if (head == null || head.next == null) {
+			return false;
+		}
+		ListNode slow = head;
+		ListNode fast = head.next;
+		while (fast != null && fast.next != null) {
+			if (fast.next == slow || fast == slow) {
+				return true;
+			}
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return false;
+	}
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
